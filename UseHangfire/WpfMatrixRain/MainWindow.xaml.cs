7a93d77cf98831bb2e8d8121faa730c3652d3bf7
laumenghui <laumenghui@gmail.com>
2021-09-16 16:10:46 +0800

    GIthub的另外矩阵雨信息检索

--- UseHangfire/WpfMatrixRain/MainWindow.xaml.cs
+++ UseHangfire/WpfMatrixRain/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfMatrixRain
 {
@@ -20,6 +22,10 @@ namespace WpfMatrixRain
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer;
+
+        WriteableBitmap writeableBitmap;
+
         /// <summary>
         /// As described by MSDN, DrawingVisual is a lightweight drawing class that is used to render shapes, 
         /// images, or text. This class is considered lightweight because it does not provide layout, input, focus, 
@@ -28,6 +34,55 @@ namespace WpfMatrixRain
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Start();
         }
+
+        const int minJpnCharCode = 0x4e00;
+        const int maxJpnCharCode = 0x4f80;
+
+        private static string GenerateRamdomChar()
+        {
+            var random = new Random();
+            return ((char)random.Next(minJpnCharCode, maxJpnCharCode)).ToString();
+
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            //FormattedText ft = new FormattedText("ss",
+            //new CultureInfo("en-us"),
+            //  FlowDirection.LeftToRight,
+            // new Typeface(new FontFamily("Arial"), FontStyles.Normal,
+            //  FontWeights.Normal, new FontStretch()),
+            //  16D,
+            //  Brushes.LimeGreen);
+
+            //DrawingVisual dv = new DrawingVisual();
+            //DrawingContext dc = dv.RenderOpen();
+            //dc.DrawText(ft, new Point(x, y));
+            canvas.Dispatcher.BeginInvoke(() => {
+                canvas.Children.Clear();
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = GenerateRamdomChar();
+                textBlock.Foreground = new SolidColorBrush(Colors.LimeGreen);
+                Canvas.SetLeft(textBlock, 10);
+                Canvas.SetTop(textBlock, 10);
+                canvas.Children.Add(textBlock);
+            });
+
+            image.Dispatcher.BeginInvoke(() =>
+            {
+                writeableBitmap = new WriteableBitmap((int)100, (int)100, 96, 96, PixelFormats.Bgr32,
+                null);          
+
+                image.Source = writeableBitmap;
+            });
+        }
+
+        
     }
 }
