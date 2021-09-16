1b9ca165121a6053b3af0f15cfb705f908650914
laumenghui <laumenghui@gmail.com>
2021-09-16 14:19:49 +0800

    App Ideas :   WPF  martix rain ；   WEB Html5  martix rain

---
+++ UseHangfire/WpfMatrixRain/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMatrixRain
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// As described by MSDN, DrawingVisual is a lightweight drawing class that is used to render shapes, 
+        /// images, or text. This class is considered lightweight because it does not provide layout, input, focus, 
+        /// or event handling, which improves its performance.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
