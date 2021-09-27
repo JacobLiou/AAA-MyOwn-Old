8a3577da78f102ff392b43f923c2673fba3ff564
laumenghui <laumenghui@gmail.com>
2021-09-27 19:26:03 +0800

    连API中的HashTable都用的少 要想想

---
+++ UseHangfire/UseCastleCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using Castle.Components.DictionaryAdapter;
+
+namespace UseCastleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var dic = new Hashtable();
+            var fac = new DictionaryAdapterFactory();
+            var ada = fac.GetAdapter<IHelloWorld>(dic);
+            dic["Message"] = "Hello World";
+            dic[1] = 2;
+            Debug.Assert(ada.Message == "Hello world!");
+        }
+    }
+
+    interface IHelloWorld
+    {
+        string Message { get; }
+    }
+}
