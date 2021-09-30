49f5c7a7d6f56d63cf53647e48301a2878ade33d
laumenghui <laumenghui@gmail.com>
2021-09-30 15:31:32 +0800

    Mock针对难以单独构建的对象进行仿制

--- UseHangfire/MockTest/UnitTest1.cs
+++ UseHangfire/MockTest/UnitTest1.cs
@@ -12,11 +12,20 @@ namespace MockTest
             var mock = new Mock<ICartService>();
             mock.Setup(cartService => cartService.AddToCart(1)).Returns(1);
             mock.Setup(cartService => cartService.AddToCart(2)).Returns(2);
+            
             var cartService = mock.Object;
 
-            var reuslt = cartService.AddToCart(2);
+            var result = cartService.AddToCart(2);
 
-            Assert.Equal(2, reuslt);
+            Assert.Equal(2, result);
+
+            //Arrange
+            mock.Setup(cartService => cartService.AddToCart(It.IsAny<int>())).Returns(3);
+            //    Act
+            result = cartService.AddToCart(200);
+            //    Assert
+            Assert.Equal(3, result);
+
         }
     }
 }
